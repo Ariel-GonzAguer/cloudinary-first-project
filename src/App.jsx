9701be26b721019bde2675e20cce914a54d03365
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2024-10-14 17:58:18 -0600

    ...

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={styles.app}>
+        <h1>Cloudinary</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
